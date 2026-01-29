5e132a2597050042fe0d1ceb913fa0ec7b95231e
Avula Lokesh <lokeshavula80@gmail.com>
2026-01-29 09:54:54 +0530

    changes

--- 01-session1.sh
+++ 01-session1.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "My DevSceOps Journer Begins!"
+USER1=Andy
+USER2=Lokesh
+
+echo "$USER1 : It works on my machine"
+echo "$USER2 : Great! Let’s make it work in production too"
+echo "$USER1 : With CI/CD?"
+echo "$USER2 : Exactly — automation over assumptions"
