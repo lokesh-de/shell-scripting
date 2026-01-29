d3263a3e76eb3f141fc67101f13cfb9df8e17f65
Avula Lokesh <lokeshavula80@gmail.com>
2026-01-29 10:09:44 +0530

    changes

---
+++ 02-svariables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "These are the special variables used in shell:"
+echo "\$0  - Script name"
+echo "\$1  - First argument"
+echo "\$2  - Second argument"
+echo "\$#  - Total number of arguments"
+echo "\$@  - All arguments"
+echo "\$*  - All arguments as single string"
+echo "\$$  - Process ID of the script"
+echo "\$?  - Exit status of last command"
+echo "Happy Learning!"
